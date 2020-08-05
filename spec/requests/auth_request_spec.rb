446befde786be7d01ad5bdc4c7d355bc21bbab84
fernandocgomez <fernandocgomez@live.com>
2020-08-04 22:24:42 -0500

    Fix Auth controller

--- spec/requests/auth_request_spec.rb
+++ spec/requests/auth_request_spec.rb
@@ -1,56 +1,50 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Auths", type: :request do
-
-  describe "POST /api/login" do
-    before(:each) do
-      @user = create_user
-      @params = {"username" => "fernandocgomez", "password" => "Ilovemytacos32%"}
-      @wrong_username_params = {"username" => "wronguser", "password" => "Ilovemytacos32%"}
-      @wrong_password_params = {"username" => "fernandocgomez", "password" => "Ilovemytacos31%"}
+  describe "#login" do
+    before(:all) do
+      @user = create(:user)
     end
-    context 'auth is succesful' do
-      it 'returns a 200 status' do
-        post "/api/login", params: @params
-
+    let(:params) { { "username" => "fernandocgomez", "password" => "Ilovemytacos32%" } }
+    let(:wrong_username_params) { { "username" => "wronguser", "password" => "Ilovemytacos32%" } }
+    let(:wrong_password_params) { { "username" => "fernandocgomez", "password" => "Ilovemytacos31%" } }
+    context "when request success" do
+      before(:each) do
+        post "/api/login", params: params
+        @resp_json = JSON(response.body)["resp"]
+      end
+      it "returns a 200 status" do
         expect(response).to have_http_status(200)
       end
-      it 'returns an object with the user instance and a auth token on JSON' do
-        post "/api/login", params: @params
-        resp_json = JSON(response.body)
-        matcher = get_user_matcher
-        matcher['id'] = resp_json['resp']['user']['id']
-        token = encode_token({user_id: @user.id})
+      it "returns user and token on JSON" do
+        matcher = get_user_matcher(@user.id)
+        token = get_auth_token(@user.id)
 
-        expect(resp_json['resp']).to match({"user" => matcher, "token" => token})
+        expect(@resp_json).to match({ "user" => matcher, "token" => token["Authorization"].split(" ")[1] })
       end
     end
-    context 'auth fails' do
-      context 'wrong username' do
-        it 'returns a 404 status' do
-          post "/api/login", params: @wrong_username_params
-  
-          expect(response).to have_http_status(404)
-        end
-        it 'returns an error message on JSON' do
-          post "/api/login", params: @wrong_username_params
-          resp_json = JSON.parse(response.body)
-
-          expect(resp_json["resp"]).to match("invalid username")
-        end
+    context "when request fails becuase of wrong username" do
+      before(:each) do
+        post "/api/login", params: wrong_username_params
+        @resp_json = JSON(response.body)["resp"]
       end
-      context 'wrong password' do
-        it 'returns a 401 status' do
-          post "/api/login", params: @wrong_password_params
-  
-          expect(response).to have_http_status(401)
-        end
-        it 'returns an error message on JSON' do
-          post "/api/login", params: @wrong_password_params
-          resp_json = JSON.parse(response.body)
-
-          expect(resp_json["resp"]).to match("invalid password")
-        end
+      it "returns a 404 status" do
+        expect(response).to have_http_status(404)
+      end
+      it "returns an error message on JSON" do
+        expect(@resp_json).to match("invalid username")
+      end
+    end
+    context "when request fails becuase of wrong password" do
+      before(:each) do
+        post "/api/login", params: wrong_password_params
+        @resp_json = JSON(response.body)["resp"]
+      end
+      it "returns a 401 status" do
+        expect(response).to have_http_status(401)
+      end
+      it "returns an error message on JSON" do
+        expect(@resp_json).to match("invalid password")
       end
     end
   end
