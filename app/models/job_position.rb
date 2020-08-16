de0987a3021e5bb64d7e3523271e78520f2c3ecb
fernandocgomez <fernandocgomez@live.com>
2020-08-15 22:45:35 -0500

    Fix companies controller tests

--- app/models/job_position.rb
+++ app/models/job_position.rb
@@ -1,7 +1,6 @@
 class JobPosition < ApplicationRecord
     has_many :lead_emails, dependent: :delete_all
     belongs_to :company
-    belongs_to :user
 
     validates_associated :lead_emails
 
--- spec/requests/companies_request_spec.rb
+++ spec/requests/companies_request_spec.rb
@@ -1,162 +1,213 @@
 require "rails_helper"
 
 RSpec.describe "Companies", type: :request do
-  before(:all) do
-    @params = get_company_params
-    @invlaid_parmas = get_invalid_cpmpany_params
-  end
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user, username: "cristobalgomez", email: "cristobal@live.com") }
 
-  describe "POST /api/companies" do
-    context "request is successful" do
+  let(:token) { get_auth_token(user.id) }
+  let(:other_user_token) { get_auth_token(other_user.id) }
+
+  let(:params) { get_company_params("valid", user.id) }
+  let(:invalid_params) { get_company_params("invalid", user.id) }
+  let(:wrong_id_params) { get_company_params("valid", other_user.id) }
+  let(:update_params) { { "name" => "the new updated name" } }
+  let(:invalid_update_params) { { "name" => nil } }
+
+  let(:company) { create(:company, user_id: user.id) }
+
+  describe "#create" do
+
+    context "when request is successful" do
+      before(:each) do
+        post "/api/companies", params: params, headers: token
+        @resp_json = parse_resp_on_json(response)
+      end
       it "returns a 201 status" do
-        post "/api/companies", params: @params
-
         expect(response).to have_http_status(201)
       end
-      it "returns an instance of the Company model on JSON" do
-        post "/api/companies", params: @params
-        resp_json = JSON.parse(response.body)
-
-        expect(resp_json["resp"]).to match(get_company_matcher(resp_json["resp"]["id"]))
+      it "returns the company instance on JSON" do
+        expect(@resp_json).to match(get_company_matcher(@resp_json["id"], user.id))
       end
-      it "saves the company on the DB" do
-        db_size = Company.all.size
-        post "/api/companies", params: @params
-
-        expect(Company.all.size).to eq(db_size + 1)
+      it "saves the company instance on the DB" do
+        expect(Company.count).to eq(1)
       end
     end
-    context "request fails" do
-      it "returns a 400 status" do
-        post "/api/companies", params: @invlaid_parmas
 
+    context "when request fails becuase of invalid params" do
+      before(:each) do
+        post "/api/companies", params: invalid_params, headers: token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it "returns a 400 status" do
         expect(response).to have_http_status(400)
       end
       it "returns an object of errors on JSON" do
-        post "/api/companies", params: @invlaid_parmas
-        resp_json = JSON.parse(response.body)
-
-        expect(resp_json["resp"]).to include("name")
-        expect(resp_json["resp"]).to_not eq({})
+        expect(@resp_json).to include("name")
+        expect(@resp_json).to_not eq({})
       end
     end
-  end
-  describe "GET /api/company/:id" do
-    before(:each) do
-      @company = create_company
-    end
-    context "request is successful" do
-      it "returns a 200 status" do
-        get "/api/company/#{@company["id"]}"
 
+    context 'when user tries to create a company not associated with its id' do
+      before(:each) do
+        post "/api/companies", params: wrong_id_params, headers: token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it 'returns a 401 status' do
+        expect(response).to have_http_status(401)
+      end
+      it 'returns an error message on JSON' do
+        expect(@resp_json).to match("you can not create a company that is not associated with your user without being an admin")
+      end
+    end
+
+  end
+
+  describe "#show" do
+
+    context "when request is successful" do
+      before(:each) do
+        get "/api/company/#{company.id}", headers: token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it "returns a 200 status" do
         expect(response).to have_http_status(200)
       end
-      it "returns an instance of the Company model on JSON based on the id provided" do
-        get "/api/company/#{@company["id"]}"
-        resp_json = JSON.parse(response.body)
-        matcher = get_company_matcher(@company["id"])
-
-        expect(resp_json["resp"]).to eq(matcher)
+      it "returns the instance of the Company model on JSON based on the id provided" do
+        expect(@resp_json).to eq(get_company_matcher(@resp_json["id"], user.id))
       end
     end
-    context "request fails" do
-      it "returns a 404 status" do
-        get "/api/company/#{@company["id"] + 1}"
 
+    context "when request fails becuase of an invalid id" do
+      before(:each) do
+        get "/api/company/#{company.id + 1}", headers: token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it "returns a 404 status" do
         expect(response).to have_http_status(404)
       end
       it "returns an error message on JSON" do
-        get "/api/company/#{@company["id"] + 1}"
-        resp_json = JSON.parse(response.body)
-
-        expect(resp_json["resp"]).to eq("company can't be found")
+        expect(@resp_json).to eq("company can't be found")
       end
     end
-  end
-  describe "PUT /api/company/:id" do
-    before(:each) do
-      @company = create_company
-      @update_params = { "name" => "Homebase" }
-      @update_invalid_params = { "name" => nil }
-    end
-    context "request is successful" do
-      it "returns a 200 status" do
-        put "/api/company/#{@company["id"]}", params: @update_params
 
+    context 'when user tries to access a company associated to another user' do
+      before(:each) do
+        get "/api/company/#{company.id}", headers: other_user_token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it 'returns a 401 status' do
+        expect(response).to have_http_status(401)
+      end
+      it 'returns an error message on JSON' do
+        expect(@resp_json).to match("you can not access to other users' company without being an admin")
+      end
+    end
+
+  end
+
+  describe "#update" do
+    
+    context "when request is successful" do
+      before(:each) do
+        put "/api/company/#{company.id}", params: update_params, headers: token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it "returns a 200 status" do
         expect(response).to have_http_status(200)
       end
       it "returns the updated instance of the Company model on JSON" do
-        put "/api/company/#{@company["id"]}", params: @update_params
-        resp_json = JSON.parse(response.body)
-        matcher = get_company_matcher(@company["id"])
-        matcher["name"] = @update_params["name"]
+        matcher = get_company_matcher(company.id, user.id)
+        matcher["name"] = update_params["name"]
 
-        expect(resp_json["resp"]).to match(matcher)
+        expect(@resp_json).to match(matcher)
       end
     end
-    context "request fails(invalid params)" do
-      it "returns a 400 status" do
-        put "/api/company/#{@company["id"]}", params: @update_invalid_params
 
+    context "when request fails becuase of invalid parmas" do
+      before(:each) do
+        put "/api/company/#{company.id}", params: invalid_update_params, headers: token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it "returns a 400 status" do
         expect(response).to have_http_status(400)
       end
       it "returns an object of errors of JSON" do
-        put "/api/company/#{@company["id"]}", params: @update_invalid_params
-        resp_json = JSON.parse(response.body)
-
-        expect(resp_json["resp"]).to include("name")
-        expect(resp_json["resp"]).to_not eq({})
+        expect(@resp_json).to include("name")
+        expect(@resp_json).to_not eq({})
       end
     end
-    context "request fails(invalid id)" do
-      it "returns a 404 status" do
-        put "/api/company/#{@company["id"] + 1}", params: @update_params
 
+    context "when request fails becuase of an invalid id" do
+      before(:each) do
+        put "/api/company/#{company.id + 1}", params: update_params, headers: token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it "returns a 404 status" do
         expect(response).to have_http_status(404)
       end
       it "returns an error message on JSON" do
-        put "/api/company/#{@company["id"] + 1}", params: @update_params
-        resp_json = JSON.parse(response.body)
-
-        expect(resp_json["resp"]).to eq("company can't be found")
+        expect(@resp_json).to eq("company can't be found")
       end
     end
-  end
-  describe "DELETE /api/company/:id" do
-    before(:each) do
-      @company = create_company
-    end
-    context "request is successful" do
-      it "returns a 200 status" do
-        delete "/api/company/#{@company["id"]}"
 
+    context 'when request fails because user tries to update a company not associated with its account' do
+      before(:each) do
+        put "/api/company/#{company.id}", params: update_params, headers: other_user_token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it 'returns a 401 status' do
+        expect(response).to have_http_status(401)
+      end
+      it 'returns an error message on JSON' do
+        expect(@resp_json).to match("you can not update a company that is not associated with your user without being an admin")   
+      end
+    end
+
+  end
+
+  describe "#destroy" do
+
+    context "when request is successful" do
+      before(:each) do
+        delete "/api/company/#{company.id}", headers: token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it "returns a 200 status" do
         expect(response).to have_http_status(200)
       end
       it "returns a confirmatino message on JSON" do
-        delete "/api/company/#{@company["id"]}"
-        resp_json = JSON.parse(response.body)
-
-        expect(resp_json["resp"]).to eq("company has been deleted")
+        expect(@resp_json).to eq("company has been deleted")
       end
       it "destroy record from the DB" do
-        db_size = Company.all.size
-        delete "/api/company/#{@company["id"]}"
-
-        expect(Company.all.size).to eq(db_size - 1)
+        expect(Company.count).to eq(0)
       end
     end
-    context "request fails" do
-      it "returns a 404 status" do
-        delete "/api/company/#{@company["id"] + 1}"
 
+    context "when request fails because of an invalid id" do
+      before(:each) do
+        delete "/api/company/#{company.id + 1}", headers: token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it "returns a 404 status" do
         expect(response).to have_http_status(404)
       end
       it "returns an error message on JSON" do
-        delete "/api/company/#{@company["id"] + 1}"
-        resp_json = JSON.parse(response.body)
-
-        expect(resp_json["resp"]).to eq("company can't be found")
+        expect(@resp_json).to eq("company can't be found")
       end
     end
+
+    context 'when a user tries to delete a company not associated with its account' do
+      before(:each) do
+        delete "/api/company/#{company.id}", headers: other_user_token
+        @resp_json = parse_resp_on_json(response)
+      end
+      it 'returns a 401 status' do
+        expect(response).to have_http_status(401)
+      end
+      it 'returns a error message on JSON' do
+        expect(@resp_json).to match("you can not delete a company that is not associated with your user without being an admin")
+      end
+    end
+    
   end
 end
